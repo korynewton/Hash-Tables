9b59795172a103ce775d524b4a59ea7525f1e55f
Kory Newton <korybnewton@gmail.com>
2019-07-08 10:37:20 -0700

    array append

--- array_linked_list.py/arrays.py
+++ array_linked_list.py/arrays.py
@@ -55,24 +55,38 @@ def array_insert(array, element, index):
 
 
 # Add an element to the end of the given array
-def array_append():
+def array_append(array, element):
 
     # Hint, this can be done with one line of code
     # (Without using a built in function)
 
     # Your code here
-    pass
+    array_insert(array, element, array.count)
 
 
 # Remove the first occurence of the given element from the array
 # Throw an error if the value is not found
-def array_remove():
+def array_remove(array, element):
     # Your code here
-    pass
+    removed = False
+    for i in range(array.count):
+        if removed:
+            array.elements[i-1] = array.elemnts[i]
+
+        elif array[i] == element:
+            removed = True
+
+    if removed:
+        array.count -= 1
+        array.elements[array.count] = None
+
+    else:
+        print('error: ' + str(element) + ' not found')
+
+        # Remove the element in a given position and return it
+        # Then shift every element after that occurrance to fill the gap
 
 
-# Remove the element in a given position and return it
-# Then shift every element after that occurrance to fill the gap
 def array_pop():
     # Throw an error if array is out of the current count
     # Your code here
